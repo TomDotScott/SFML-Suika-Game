cfc05e28a8d052eaed0fca53a560f76603fb50d4
Tom Scott <tom.scott45456@gmail.com>
2025-01-07 22:09:21 +0000

    Sharpmake generates solution linking SFML
    I hate sharpmake. I originally wanted to build SFML from source but didn't realise the mammoth undertaking that is so have replicated the tutorial using Sharpmake. Links the static libs and builds CPP17

--- Main.Sharpmake.cs
+++ Main.Sharpmake.cs
@@ -1,12 +1,24 @@
+using System.IO;
+using System.Runtime.CompilerServices;
 using Sharpmake;
 
+[module: Sharpmake.Include("Constants.cs")]
 [module: Sharpmake.Include("SuikaGame.Sharpmake.cs")]
 
 public static class Main
 {
+    private static string GetThisFilePath([CallerFilePath] string path = null)
+    {
+        return path;
+    }
+
+
     [Sharpmake.Main]
     public static void SharpmakeMain(Sharpmake.Arguments arguments)
     {
+        // Find the folder of this Csharp file
+        Constants.PROJECT_ROOT = Path.GetDirectoryName(GetThisFilePath());
+
         arguments.Generate<SuikaGameSolution>();
     }
 }
--- SuikaGame.Sharpmake.cs
+++ SuikaGame.Sharpmake.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using System.IO;
 using Sharpmake;
 
@@ -21,10 +22,77 @@ public class SuikaGame : Project
         );
     }
 
+    // Probably a bit overkill to have a class for this but it does the job
+    class SFML_Dependency
+    {
+        public string LibraryName;
+        public bool IsSFMLLibrary;
+
+        public SFML_Dependency(string libraryName, bool isSFMLLibrary = false)
+        {
+            LibraryName = libraryName;
+
+            IsSFMLLibrary = isSFMLLibrary;
+        }
+    }
+
+    private void AddSfmlDependency(SFML_Dependency dependency, Configuration conf, Target target)
+    {
+        string libraryName = dependency.LibraryName;
+
+        // Static debug SFML libraries end in -s-d.lib... Yay...
+        if (dependency.IsSFMLLibrary)
+        {
+            libraryName += "-s";
+
+            if (target.Optimization == Optimization.Debug)
+            {
+                libraryName += "-d";
+            }
+        }
+
+        libraryName += ".lib";
+        conf.LibraryFiles.Add(libraryName);
+    }
+
     [Configure]
-    public void ConfigureAll(Project.Configuration conf, Target target)
+    public void ConfigureAll(Configuration conf, Target target)
     {
         conf.ProjectPath = $"build_{target.DevEnv}";
+
+        conf.Options.Add(Options.Vc.Compiler.CppLanguageStandard.CPP17);
+
+        conf.IncludePaths.Add(Path.Combine(Constants.PROJECT_ROOT, "SFML", "include"));
+
+        conf.LibraryPaths.Add(Path.Combine(Constants.PROJECT_ROOT, "SFML", "lib"));
+
+        // This sucks. Maybe a better data structure would work but I am fed up with Sharpmake
+        var sfmlDependencies = new Dictionary<SFML_Dependency, List<SFML_Dependency>>
+        {
+            { new("sfml-graphics", true), new List<SFML_Dependency> { new("sfml-window", true), new("sfml-system", true), new("opengl32"), new("freetype") } },
+            { new("sfml-window", true), new List<SFML_Dependency>{ new("sfml-system", true), new("opengl32"), new("winmm"), new("gdi32") } },
+            { new("sfml-audio", true), new List<SFML_Dependency>{ new("sfml-system", true), new("flac"), new("vorbisenc"), new("vorbisfile"), new("vorbis"), new("ogg") } },
+            { new("sfml-network", true), new List<SFML_Dependency>{ new("sfml-system", true), new("ws2_32") } },
+            { new("sfml-system", true), new List<SFML_Dependency> { new("winmm") } }
+        };
+
+        foreach (var pair in sfmlDependencies)
+        {
+            AddSfmlDependency(pair.Key, conf, target);
+
+            foreach (var dependency in pair.Value)
+            {
+                AddSfmlDependency(dependency, conf, target);
+            }
+        }
+
+        conf.Options.Add(target.Optimization == Optimization.Debug
+            ? Options.Vc.Compiler.RuntimeLibrary.MultiThreadedDebugDLL
+            : Options.Vc.Compiler.RuntimeLibrary.MultiThreadedDLL);
+
+        conf.Options.Add(Options.Vc.Compiler.Exceptions.Enable);
+
+        conf.Defines.Add("SFML_STATIC");
     }
 }
 
@@ -47,11 +115,10 @@ public class SuikaGameSolution : Solution
     }
 
     [Configure]
-    public void ConfigureAll(Solution.Configuration conf, Target target)
+    public void ConfigureAll(Configuration conf, Target target)
     {
         conf.SolutionPath = "[solution.SharpmakeCsPath]";
 
         conf.AddProject<SuikaGame>(target);
     }
 }
-
